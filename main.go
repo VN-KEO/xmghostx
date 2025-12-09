436668d4ea02d16eb53dae886c12cf0a11b9ed16
KEO <92074854+VN-KEO@users.noreply.github.com>
2025-12-09 12:24:39 +0100

    Create main.go
    (Simple test server)

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "MGHOSTX Server is running!")
+	})
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "OK")
+	})
+
+	fmt.Println("Starting MGHOSTX server on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
